5cfa1449f3e6445c22210a8ecb7edd13dd7ffa07
Samuel Lee <sam.leefr@gmail.com>
2023-11-07 16:59:03 +0100

    Gateway Commit

---
+++ src/main/java/org/capitalcompass/capitalcompassgateway/CapitalCompassGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.capitalcompass.capitalcompassgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalCompassGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalCompassGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/capitalcompass/capitalcompassgateway/CapitalCompassGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.capitalcompass.capitalcompassgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalCompassGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
