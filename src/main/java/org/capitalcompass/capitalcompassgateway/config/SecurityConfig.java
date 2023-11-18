eb4b536d0f2e23d984279ae8b0a6e003e26e3cee
Samuel Lee <sam.leefr@gmail.com>
2023-11-18 15:49:14 +0100

    route

--- src/main/java/org/capitalcompass/capitalcompassgateway/config/SecurityConfig.java
+++ src/main/java/org/capitalcompass/capitalcompassgateway/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable()
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/users/*").permitAll()
+                        .pathMatchers("/users/*", "/stocks/*").permitAll()
                         .anyExchange().authenticated()
                 )
                 .exceptionHandling(exceptionHandling -> exceptionHandling
