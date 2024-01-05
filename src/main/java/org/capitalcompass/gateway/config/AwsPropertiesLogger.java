9837c7b452a48751f7f7220d4a7327f135cdfc8e
Samuel Lee <sam.leefr@gmail.com>
2024-01-05 13:33:48 +0100

    spring cloud ssl, SameSite=None; Secure draft

---
+++ src/main/java/org/capitalcompass/gateway/config/AwsPropertiesLogger.java
@@ -0,0 +1,23 @@
+package org.capitalcompass.gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class AwsPropertiesLogger implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final Environment env;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        String webAppUrl = env.getProperty("frontend.url");
+
+        log.info("frontend.url: {}", webAppUrl);
+    }
+}
--- src/main/java/org/capitalcompass/gateway/config/SecurityConfig.java
+++ src/main/java/org/capitalcompass/gateway/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package org.capitalcompass.gateway.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -21,16 +23,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Value("${webapp.url}")
+    @Value("${frontend.url}")
     private String webAppUrl;
 
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable()
                 .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
                         .pathMatchers("/v1/users/**", "/v1/stocks/**", "/actuator/**").permitAll()
                         .anyExchange().authenticated()
                 )
+                .redirectToHttps(Customizer.withDefaults())
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .oauth2Login(oauth2Login -> oauth2Login
