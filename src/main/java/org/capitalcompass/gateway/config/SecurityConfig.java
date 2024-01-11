4ab7f81cac758a5b3dd27e6c778ae19803cfe7f3
Samuel Lee <sam.leefr@gmail.com>
2024-01-11 22:16:00 +0100

    clean up

--- src/main/java/org/capitalcompass/gateway/config/SecurityConfig.java
+++ src/main/java/org/capitalcompass/gateway/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.nimbusds.jose.shaded.json.JSONArray;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
@@ -32,6 +33,7 @@ import java.util.List;
 import java.util.Set;
 
 
+@Configuration
 @EnableWebFluxSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
