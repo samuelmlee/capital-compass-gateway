d98fb1d8f532c43243aa0ced3061686e31916741
Samuel Lee <sam.leefr@gmail.com>
2024-01-11 21:54:46 +0100

    clean up

--- src/main/java/org/capitalcompass/gateway/GatewayApplication.java
+++ src/main/java/org/capitalcompass/gateway/GatewayApplication.java
@@ -1,25 +1,15 @@
 package org.capitalcompass.gateway;
 
 import lombok.RequiredArgsConstructor;
-import org.capitalcompass.gateway.service.KeycloakAdminService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class GatewayApplication implements CommandLineRunner {
-
-    private final KeycloakAdminService keycloakAdminService;
+public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        keycloakAdminService.getUsers();
-
     }
 }
